12e6aa5065a432aea51f490f1877781cccaca336
reuben thomas <reuben@debian>
2014-05-20 10:55:38 +0100

    updated example so that it's not too dumb any more

--- src/Explosion.hs
+++ src/Explosion.hs
@@ -1,16 +1,16 @@
 module Explosion where
 
-import Pipes.Parse (foldAll, yield, Parser, Producer)
-import Pipes.ByteString (ByteString)
+import Pipes.Parse (foldAll, Parser, Producer)
+import Pipes.ByteString (ByteString, fromLazy)
 import Pipes.Aeson (DecodingError)
 import Pipes.Aeson.Unchecked (decoded)
 import Data.List (intercalate)
-import Data.ByteString.Char8 (pack)
+import Data.ByteString.Lazy.Char8 (pack)
 import Lens.Family (view)
 import Lens.Family.State.Strict (zoom)
 
 produceString :: Producer ByteString IO ()
-produceString = yield $ pack $ intercalate " " $ map show [1..1000000]
+produceString = fromLazy $ pack $ intercalate " " $ map show [1..1000000]
 
 produceInts :: Producer Int IO (Either (DecodingError, Producer ByteString IO ()) ())
 produceInts = view decoded produceString
