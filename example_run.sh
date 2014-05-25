45295db2b9dfa05ed75bbfdc17a1e63719117502
reuk <reuben@null.local>
2014-05-25 13:58:53 +0200

    updated run script

--- example_run.sh
+++ example_run.sh
@@ -1,1 +1,3 @@
-~/.cabal/bin/test0 +RTS -h; ~/.cabal/bin/test1 +RTS -h; hp2ps -c test0.hp ; hp2ps -c test1.hp
+test0 +RTS -h; test1 +RTS -h; hp2ps -c test0.hp ; hp2ps -c test1.hp
+
+test2 trace.json +RTS -h -p -sstderr; hp2ps -c test2.hp
